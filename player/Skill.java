8273f2e6bae0dab01ccd3c7820e4b19ef9e53316
rburns31 <rburns31@gatech.edu>
2014-11-11 12:43:01 -0500

    JAVADOC

--- player/Skill.java
+++ player/Skill.java
@@ -2,45 +2,75 @@ package spacetrader.player;
 
 import java.io.Serializable;
 
+/**
+ * 
+ * @author Team TYN
+ */
 public class Skill implements Serializable {
+    /**
+     * 
+     */
     private String type;
+    /**
+     * 
+     */
     private int value;
 
     /**
-     * 
-     * @param type 
+     * Single arg constructor that passes 0 for the value.
+     * @param aType The type of skill to be created
      */
-    public Skill(String type) {
-        this(type, 0);
+    public Skill(final String aType) {
+        this(aType, 0);
     }
 
     /**
-     * 
-     * @param type
-     * @param value 
+     * Two arg constructor that sets an initial value for the skill.
+     * @param aType The type of skill to be created
+     * @param aValue The initial value for the skill
      */
-    public Skill(String type, int value) {
-        this.type = type;
-        this.value = value;
+    public Skill(final String aType, final int aValue) {
+        type = aType;
+        value = aValue;
     }
 
     // Getter for value
-    public int getValue() {
+
+    /**
+     *
+     * @return
+     */
+    public final int getValue() {
         return this.value;
     }
 
     // Getter for type
-    public String getType() {
+
+    /**
+     *
+     * @return
+     */
+    public final String getType() {
         return this.type;
     }
 
     // Setter for value
-    public void setValue(int newValue) {
+
+    /**
+     *
+     * @param newValue
+     */
+    public final void setValue(int newValue) {
         this.value = newValue;
     }
 
     // Increase the value of skill by specified amount
-    public void increaseValue(int value) {
-        this.value += value;
+
+    /**
+     *
+     * @param moreValue The value to be added the skill's value
+     */
+    public final void increaseValue(int moreValue) {
+        this.value += moreValue;
     }
 }
