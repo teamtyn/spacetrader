507104bdb1e68a9cdcd46935683c6414bf79d54d
Nico de Leon <nico@ns.gg>
2014-11-10 19:31:15 -0500

    Make SerializableColor checkstylish

--- ui/SerializableColor.java
+++ ui/SerializableColor.java
@@ -7,26 +7,27 @@ import java.io.Serializable;
 import javafx.scene.paint.Color;
 
 /**
- * SerializableColor is a class used to store colors in a serializable way
- * 
+ * SerializableColor is a class used to store colors in a serializable way.
+ *
  * @author Team TYN
  */
-public class SerializableColor implements Serializable {
+public final class SerializableColor implements Serializable {
 
+    /**
+     * The (non-serializable) JavaFX color object that is wrapped by the class.
+     */
     private transient Color color;
 
     /**
-     * Constructor for SerializableColor
-     * 
-     * @param color the color that we are storing
+     * Constructor for SerializableColor.
+     *
+     * @param aColor the color that we are storing
      */
-    public SerializableColor(Color color) {
-        this.color = color;
+    public SerializableColor(final Color aColor) {
+        this.color = aColor;
     }
 
     /**
-     * Getter for the color
-     * 
      * @return the color
      */
     public Color getColor() {
@@ -35,23 +36,28 @@ public class SerializableColor implements Serializable {
 
     /**
      * Method to read in the color from the storage file.
-     * 
+     *
      * @param in the input stream being read from
      * @throws IOException thrown if problem with the file
      * @throws ClassNotFoundException thrown if problem with the class
      */
-    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+    private void readObject(final ObjectInputStream in)
+            throws IOException, ClassNotFoundException {
         in.defaultReadObject();
-        color = Color.color(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble());
+        color = Color.color(
+                in.readDouble(),
+                in.readDouble(),
+                in.readDouble(),
+                in.readDouble());
     }
 
     /**
      * Method to write the color to the storage file.
-     * 
+     *
      * @param out the Output stream being written to
      * @throws IOException thrown if problem with the file
      */
-    private void writeObject(ObjectOutputStream out) throws IOException {
+    private void writeObject(final ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
         out.writeDouble(color.getRed());
         out.writeDouble(color.getGreen());
