81567bedc33b68eb065e85b6e724de8f4147208b
Nico de Leon <nico@ns.gg>
2014-11-11 23:52:59 -0500

    Checkstylize Government

--- star_system/Government.java
+++ star_system/Government.java
@@ -5,26 +5,85 @@ import java.util.HashMap;
 import spacetrader.GameModel;
 
 /**
- * Government is defined by its type, leader, and level of anger
+ * Government is defined by its type, leader, and level of anger.
  *
  * @author David Purcell
  */
-public class Government implements Serializable {
+public final class Government implements Serializable {
 
+    /**
+     * All the possible kinds of government.
+     */
     public enum Type {
-        ANARCHY, ARISTOCRACY, CAPITALIST, COMMUNIST,
-        CORPORATE, DEMOCRACY, FASCIST, MERITOCRACY,
-        MONARCHY, OLIGARCHY, TECHNOCRACY, THEOCRACY
+
+        /**
+         * No government.
+         */
+        ANARCHY,
+        /**
+         * Government run by the upper class families.
+         */
+        ARISTOCRACY,
+        /**
+         * Government run by money.
+         */
+        CAPITALIST,
+        /**
+         * Government run "for the people", ostensibly.
+         */
+        COMMUNIST,
+        /**
+         * Government run by big companies.
+         */
+        CORPORATE,
+        /**
+         * Government run by big companies.
+         */
+        DEMOCRACY,
+        /**
+         * Government run by misled patriotism.
+         */
+        FASCIST,
+        /**
+         * Government run by those who think they can judge skill objectively.
+         */
+        MERITOCRACY,
+        /**
+         * Government run by those who have had a farcical aquatic ceremony.
+         */
+        MONARCHY,
+        /**
+         * Government run by the upper class.
+         */
+        OLIGARCHY,
+        /**
+         * Government run by those who wish to recreate the Matrix.
+         */
+        TECHNOCRACY,
+        /**
+         * Government run by Madoka.
+         */
+        THEOCRACY
     };
+    /**
+     * The leaders of each government.
+     */
     private final HashMap<Type, String> leaders;
+    /**
+     * The type of this particular government.
+     */
     private Type type;
+    /**
+     * The leader of this particular government.
+     */
     private String leader;
 
     /**
      * No argument constructor for a Government.
      */
     public Government() {
-        type = Type.values()[GameModel.getRandom().nextInt(Type.values().length)];
+        type = Type
+                .values()[GameModel.getRandom().nextInt(Type.values().length)];
         leaders = new HashMap<>();
         //setUpLeaderMap();
         //leader = leaders.get(type);
@@ -51,14 +110,16 @@ public class Government implements Serializable {
 
     /**
      * Getter for the leader of the government.
+     *
      * @return The leader of the government
      */
-    public final String getLeader() {
+    public String getLeader() {
         return leader;
     }
 
     /**
      * Setter for the new leader of the government.
+     *
      * @param newLeader The new leader
      */
     public void setLeader(final String newLeader) {
@@ -67,14 +128,16 @@ public class Government implements Serializable {
 
     /**
      * Getter for the type of the government.
+     *
      * @return The type of the government
      */
-    public final Type getType() {
+    public Type getType() {
         return type;
     }
 
     /**
      * Sets the government type to a new type.
+     *
      * @param newType The new government type
      */
     public void setType(final Type newType) {
@@ -82,7 +145,7 @@ public class Government implements Serializable {
     }
 
     @Override
-    public final String toString() {
+    public String toString() {
         String str = "Government Type: " + type + "\nLeader: " + leader;
         return str;
     }
