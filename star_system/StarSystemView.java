a556a6669b9adc26b821a604dafa42c9c8535643
rburns31 <rburns31@gatech.edu>
2014-11-11 23:25:42 -0500

    JAVADOC
    JOSH FIX THIS

--- star_system/StarSystemView.java
+++ star_system/StarSystemView.java
@@ -14,21 +14,42 @@ import spacetrader.Xform;
 import spacetrader.Xform.RotateOrder;
 
 /**
- *
+ * 
  * @author Team TYN
  */
-public class StarSystemView extends Sphere {
-
+public final class StarSystemView extends Sphere {
+    /**
+     * 
+     */
     private final StarSystem system;
+    /**
+     * 
+     */
     private final ArrayList<PlanetView> planetViews;
+    /**
+     * 
+     */
     private final Xform systemXform;
+    /**
+     * 
+     */
     private final Xform planetsXform;
+    /**
+     * 
+     */
     private final PointLight light;
+    /**
+     * 
+     */
     private final PhongMaterial material;
 
-    public StarSystemView(StarSystem system) {
-        super(system.getSize());
-        this.system = system;
+    /**
+     * 
+     * @param aSystem 
+     */
+    public StarSystemView(final StarSystem aSystem) {
+        super(aSystem.getSize());
+        system = aSystem;
         planetViews = new ArrayList<>(system.getPlanets().length);
         systemXform = new Xform(RotateOrder.ZYX);
         planetsXform = new Xform();
@@ -54,72 +75,134 @@ public class StarSystemView extends Sphere {
         UniverseMapSubScene.NO_SHADE.getScope().add(this);
     }
 
+    /**
+     * Gets the star system associated with this star system view.
+     * @return The star system associated with this star system view
+     */
     public StarSystem getSystem() {
         return system;
     }
 
+    /**
+     * Gets the planet views associated with this star system view.
+     * @return The planet views associated with this star system view
+     */
     public ArrayList<PlanetView> getPlanetViews() {
         return planetViews;
     }
 
-    public boolean containsPlanet(PlanetView planet) {
+    /**
+     * Tests whether this planet is already contained.
+     * @param planet To be tested for containment
+     * @return Whether the planet views contains this planet
+     */
+    public boolean containsPlanet(final PlanetView planet) {
         return planetViews.contains(planet);
     }
 
+    /**
+     * Gets the system x form associated with this star system view.
+     * @return The system x form associated with this star system view
+     */
     public Xform getSystemXform() {
         return systemXform;
     }
 
+    /**
+     * Gets the planets x form associated with this star system view.
+     * @return The planets x form associated with this star system view
+     */
     public Xform getPlanetsXform() {
         return planetsXform;
     }
 
+    /**
+     * Gets the x associated with this star system view.
+     * @return The x associated with this star system view
+     */
     public double getX() {
         return systemXform.getTx();
     }
 
+    /**
+     * Gets the y associated with this star system view.
+     * @return The y associated with this star system view
+     */
     public double getY() {
         return systemXform.getTy();
     }
 
+    /**
+     * Gets the z associated with this star system view.
+     * @return The z associated with this star system view
+     */
     public double getZ() {
         return systemXform.getTz();
     }
 
+    /**
+     * Gets the rx associated with this star system view.
+     * @return The rx associated with this star system view
+     */
     public double getRx() {
         return systemXform.getRx();
     }
 
+    /**
+     * Gets the ry associated with this star system view.
+     * @return The ry associated with this star system view
+     */
     public double getRy() {
         return systemXform.getRy();
     }
 
+    /**
+     * Gets the rz associated with this star system view.
+     * @return The rz associated with this star system view
+     */
     public double getRz() {
         return systemXform.getRz();
     }
 
-    public void setLightOn(boolean on) {
+    /**
+     * Delegates to the setLightOn() method of light.
+     * @param on Whether to turn the light on or not
+     */
+    public void setLightOn(final boolean on) {
         light.setLightOn(on);
     }
 
+    /**
+     * Expands all of the planet views.
+     */
     public void expand() {
         for (PlanetView planet : planetViews) {
             planet.expand();
         }
     }
 
+    /**
+     * Collapses all of the planet views.
+     */
     public void collapse() {
         for (PlanetView planet : planetViews) {
             planet.collapse();
         }
     }
 
-    public void updateTextures(int width, int height) {
-        ExecutorService es = Executors.newSingleThreadExecutor(new ThreadFactory() {
+    /**
+     * 
+     * @param width
+     * @param height 
+     */
+    public void updateTextures(final int width, final int height) {
+        ExecutorService es = Executors.newSingleThreadExecutor(
+                new ThreadFactory() {
 
             @Override
-            public Thread newThread(Runnable runnable) {
-                Thread thread = Executors.defaultThreadFactory().newThread(runnable);
+            public Thread newThread(final Runnable runnable) {
+                Thread thread =
+                        Executors.defaultThreadFactory().newThread(runnable);
                 thread.setDaemon(true);
                 return thread;
             }
@@ -130,6 +213,9 @@ public class StarSystemView extends Sphere {
         es.shutdown();
     }
 
+    /**
+     * 
+     */
     public void incrementOrbits() {
         for (PlanetView planet : planetViews) {
             planet.incrementOrbit();
