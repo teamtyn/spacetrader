92246eea693a8e5a4e1aac6748ad414ca975f76c
ckucera3 <claytonkucera@gmail.com>
2014-12-03 22:09:16 -0500

    display for cpu

--- ComputerController.java
+++ ComputerController.java
@@ -6,15 +6,18 @@
 
 package spacetrader;
 
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.WindowEvent;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Rectangle;
 /**
  *
  * @author Local Clayton
@@ -25,28 +28,66 @@ public class ComputerController {
         currParent = parent;
         System.out.println(parent);
         modifyParent(parent);
-        VBox box = setUpBox();
-        parent.getChildren().add(box);
+        StackPane view = setUpView();
+        parent.getChildren().add(view);
         return parent;
     }
     
-    public static void closeCPUView(Pane parent, Pane cpuView) {
-        
+    public static void closeCPUView(StackPane stack) {
+        currParent.getChildren().remove(stack);
     }
     
-    public static VBox setUpBox() {
-        VBox box = new VBox();
+    private static StackPane setUpView() {
+        StackPane stack = createStackPane();
+        Rectangle rect = createRectangle();
+        VBox box = setUpBox(stack);
+        stack.getChildren().setAll(rect, box);
+        return stack;
+    }
+    
+    private static StackPane createStackPane() {
+        StackPane stack = new StackPane();
+        stack.minHeight((currParent.getLayoutBounds().getHeight()/ 2));
+        stack.minWidth((currParent.getLayoutBounds().getWidth()/ 2));
+        stack.maxHeight(Double.MAX_VALUE);
+        stack.maxWidth(Double.MAX_VALUE);
+        stack.setLayoutX((currParent.getLayoutBounds().getWidth() / 4));
+        stack.setLayoutY((currParent.getLayoutBounds().getHeight() / 4));
+        stack.setStyle("-fx-background-color: rgb(0,3,80)");
+
+        return stack;
+    }
+
+    
+    private static Rectangle createRectangle() {
+        Rectangle box = new Rectangle((currParent.getLayoutBounds().getWidth() / 2),
+                (currParent.getLayoutBounds().getHeight() / 2));
+        box.maxHeight(Double.MAX_VALUE);
+        box.maxWidth(Double.MAX_VALUE);
+        box.setLayoutX((currParent.getLayoutBounds().getWidth()) - (box.getWidth() / 2));
+        box.setLayoutY((currParent.getLayoutBounds().getHeight()) - (box.getHeight() / 2));
+        box.setStyle("-fx-background-color: rgb(50,3,80)");
+        box.setStyle("-fx-border-color: rgb(100,100,100)");
+//        box.styleProperty().bind(Bindings
+//            .concat("-fx-background-radius:200;"));
         
-        box = establishBoxProperties(box);
-        propogateElementsInBox(box);
         return box;
     }
     
-    public static void propogateElementsInBox(VBox box) {
+    public static VBox setUpBox(StackPane stack) {
+        VBox box = new VBox();
+        
+        box = establishBoxProperties(box);
+        propogateElementsInBox(box, stack);
+        return box;
+    }
+    
+    public static void propogateElementsInBox(VBox box, StackPane stack) {
         box.getChildren().clear();
-        box.getChildren().add(new Label("HELLO CAPTAIN " + GameModel.getPlayer().getName() + ", I AM COMPUTER."));
+        box.getChildren().add(new Label("HELLO CAPTAIN " 
+                + GameModel.getPlayer().getName() + ", I AM COMPUTER."));
         box.getChildren().add(createMuteButton());
-        box.getChildren().add(createBackButton(box));
+        box.getChildren().add(createBackButton(stack));
     }
     
     public static VBox establishBoxProperties(VBox box) {
@@ -113,12 +154,12 @@ public class ComputerController {
         butt.setText("UNMUTE");
     }
     
-    private static Button createBackButton(VBox box) {
+    private static Button createBackButton(StackPane stack) {
         Button backButton = new Button("BACK");
         backButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(final ActionEvent event) {
-                currParent.getChildren().remove(box);
+                closeCPUView(stack);
             }
         });
         return backButton;
